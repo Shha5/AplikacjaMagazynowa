ca8294089d452b054d602fa272cbd12298f8522e
Ewa Szczepska <ewapietrek@outlook.com>
2023-08-25 16:24:49 +0300

    Added db tables

---
+++ AplikacjaMagazynowaData/dbo/Tables/OrderDetail.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[OrderDetails]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+	[OrderId] INT NOT NULL,
+	[ProductId] INT NOT NULL, 
+	[Quantity] INT NOT NULL DEFAULT 1,
+	[OrderDate] DATETIME2 NOT NULL,
+	[LastModifiedDate] DATETIME2 NOT NULL DEFAULT getutcdate(), 
+    CONSTRAINT [FK_OrderDetails_Order] FOREIGN KEY ([OrderId]) REFERENCES [Order]([Id]), 
+    CONSTRAINT [FK_OrderDetails_Product] FOREIGN KEY ([ProductId]) REFERENCES [Product]([Id])
+)
---
+++ AplikacjaMagazynowaData/dbo/Tables/Product.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Product]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+	[ProductCode] NVARCHAR(50) NOT NULL,
+	[ProductName] NVARCHAR(100) NOT NULL,
+	[QuantityInStock] INT NOT NULL DEFAULT 1,
+	[CreatedDate] DATETIME2 NOT NULL DEFAULT getutcdate(),
+	[LastModifiedDate] DATETIME2 NOT NULL DEFAULT getutcdate()
+)
